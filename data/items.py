49310facc730935a8189b26dc0cf913c9bc7bc74
xaniyaa <ostaninmiha7@gmail.com>
2023-04-16 20:01:37 +0300

    new items

--- data/items.py
+++ data/items.py
@@ -4,6 +4,17 @@ item_dict_ru = {
     'T6_ARMOR_PLATE_SET3' : 'Броня Хранителя 6.0',
     'T7_ARMOR_PLATE_SET3' : 'Броня Хранителя 7.0',
     'T8_ARMOR_PLATE_SET3' : 'Броня Хранителя 8.0',
+    'T4_ARMOR_LEATHER_SET' : 'Куртка убийцы 4.0',
+    'T5_ARMOR_LEATHER_SET' : 'Куртка убийцы 5.0',
+    'T6_ARMOR_LEATHER_SET' : 'Куртка убийцы 6.0',
+    'T7_ARMOR_LEATHER_SET' : 'Куртка убийцы 7.0',
+    'T8_ARMOR_LEATHER_SET' : 'Куртка убийцы 8.0',
+    'T4_ARMOR_LEATHER_SET1' : 'Куртка наемника 4.0',
+    'T5_ARMOR_LEATHER_SET1' : 'Куртка наемника 5.0',
+    'T6_ARMOR_LEATHER_SET1' : 'Куртка наемника 6.0',
+    'T7_ARMOR_LEATHER_SET1' : 'Куртка наемника 7.0',
+    'T8_ARMOR_LEATHER_SET1' : 'Куртка наемника 8.0',
+    
 }
 
 item_dict_inverted = {
@@ -12,6 +23,17 @@ item_dict_inverted = {
     'Броня Хранителя 6.0' : 'T6_ARMOR_PLATE_SET3',
     'Броня Хранителя 7.0' : 'T7_ARMOR_PLATE_SET3',
     'Броня Хранителя 8.0' : 'T8_ARMOR_PLATE_SET3',
+    'Куртка убийцы 4.0' : 'T4_ARMOR_LEATHER_SET',
+    'Куртка убийцы 5.0' : 'T5_ARMOR_LEATHER_SET',
+    'Куртка убийцы 6.0' : 'T6_ARMOR_LEATHER_SET',
+    'Куртка убийцы 7.0' : 'T7_ARMOR_LEATHER_SET',
+    'Куртка убийцы 8.0' : 'T8_ARMOR_LEATHER_SET',
+    'Куртка наемника 4.0' : 'T4_ARMOR_LEATHER_SET1',
+    'Куртка наемника 5.0' : 'T5_ARMOR_LEATHER_SET1',
+    'Куртка наемника 6.0' : 'T6_ARMOR_LEATHER_SET1',
+    'Куртка наемника 7.0' : 'T7_ARMOR_LEATHER_SET1',
+    'Куртка наемника 8.0' : 'T8_ARMOR_LEATHER_SET1',
+    
 }
 
 locations = ['Lymhurst', 'BlackMarket', 'Thetford', 'FortSterling', 'Martlock', 'Bridgewatch']
@@ -30,5 +52,15 @@ item_list = [
     'Броня Хранителя 6.0',
     'Броня Хранителя 7.0',
     'Броня Хранителя 8.0',
+    'Куртка убийцы 4.0',
+    'Куртка убийцы 5.0',
+    'Куртка убийцы 6.0',
+    'Куртка убийцы 7.0',
+    'Куртка убийцы 8.0',
+    'Куртка наемника 4.0',
+    'Куртка наемника 5.0',
+    'Куртка наемника 6.0',
+    'Куртка наемника 7.0',
+    'Куртка наемника 8.0',
 ]
 
